1b8a2ed3f477f9cda5783e7628242adc67a3b40f
fujikawahiroaki <fhir0aki3@gmail.com>
2024-06-18 04:44:48 +0900

    feat: APIフィルタリング補助マクロにPostGISのPoint2Dに対する検索を追加
    - 緯度経度を数値と見なして=, <=, >=検索を追加
    - 指定した座標から指定半径内にある座標カラムの検索を追加

--- src/actions/mixins/api/custom/filter_macros.cr
+++ src/actions/mixins/api/custom/filter_macros.cr
@@ -45,6 +45,62 @@ module Api::Custom::FilterMacros
     {% end %}
   end
 
+  macro geo_point_chain_filters(query, *columns)
+    {% for column in columns %}
+      %longitude_eq_sym = :{{column}}__longitude
+      %latitude_eq_sym = :{{column}}__latitude
+      %longitude_lte_sym = :{{column}}__longitude_lte
+      %longitude_gte_sym = :{{column}}__longitude_gte
+      %latitude_lte_sym = :{{column}}__latitude_lte
+      %latitude_gte_sym = :{{column}}__latitude_gte
+      if params.get?(%longitude_eq_sym)
+        if params.get(%longitude_eq_sym).to_f?
+          {{query}} = {{query}}.{{column}}.x.eq(params.get(%longitude_eq_sym))
+        end
+      end
+      if params.get?(%latitude_eq_sym)
+        if params.get(%latitude_eq_sym).to_f?
+          {{query}} = {{query}}.{{column}}.y.eq(params.get(%latitude_eq_sym))
+        end
+      end
+      if params.get?(%longitude_lte_sym)
+        if params.get(%longitude_lte_sym).to_f?
+          {{query}} = {{query}}.{{column}}.x.lte(params.get(%longitude_lte_sym))
+        end
+      end
+      if params.get?(%longitude_gte_sym)
+        if params.get(%longitude_gte_sym).to_f?
+          {{query}} = {{query}}.{{column}}.x.gte(params.get(%longitude_gte_sym))
+        end
+      end
+      if params.get?(%latitude_lte_sym)
+        if params.get(%latitude_lte_sym).to_f?
+          {{query}} = {{query}}.{{column}}.y.lte(params.get(%latitude_lte_sym))
+        end
+      end
+      if params.get?(%latitude_gte_sym)
+        if params.get(%latitude_gte_sym).to_f?
+          {{query}} = {{query}}.{{column}}.y.gte(params.get(%latitude_gte_sym))
+        end
+      end
+    {% end %}
+  end
+
+  macro geo_point_distance_filters(query, *columns)
+    %distance_longitude_sym = :__distance_longitude
+    %distance_latitude_sym = :__distance_latitude
+    %distance_radius_gte_sym = :__distance_radius_gte
+    %distance_radius_lte_sym = :__distance_radius_lte
+    {% for column in columns %}
+      if params.get?(%distance_longitude_sym) && params.get?(%distance_latitude_sym) && params.get?(%distance_radius_lte_sym)
+        {{query}} = {{query}}.{{column}}.distance(params.get(%distance_latitude_sym), params.get(%distance_latitude_sym)).lte(params.get(%distance_radius_lte_sym))
+      end
+      if params.get?(%distance_longitude_sym) && params.get?(%distance_latitude_sym) && params.get?(%distance_radius_gte_sym)
+        {{query}} = {{query}}.{{column}}.distance(params.get(%distance_latitude_sym), params.get(%distance_latitude_sym)).gte(params.get(%distance_radius_gte_sym))
+      end
+    {% end %}
+  end
+
   macro ordering_single_column_query(query, ordering_sym, default_column, *allowed_columns)
     if params.get?({{ordering_sym}})
       %ordering = params.get({{ordering_sym}})
