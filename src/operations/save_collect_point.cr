07fd0634a54b27590415daa4c1b1f4aab0436170
fujikawahiroaki <fhir0aki3@gmail.com>
2024-06-29 19:04:37 +0900

    feat: 採集地点のoperationにバリデーションを追加

--- src/operations/save_collect_point.cr
+++ src/operations/save_collect_point.cr
@@ -1,4 +1,6 @@
 require "./mixins/save_base64_image"
+require "./mixins/countries"
+require "./mixins/regex"
 
 class SaveCollectPoint < CollectPoint::SaveOperation
   permit_columns(
@@ -25,10 +27,39 @@ class SaveCollectPoint < CollectPoint::SaveOperation
     image2,
     image3,
     image4,
-    image5,
+    image5
   )
 
   before_save do
+    validate_size_of contient, max: 20
+    validate_format_of contient, with: ValidationRegex.ascii
+
+    validate_size_of island_group, max: 30
+    validate_format_of island_group, with: ValidationRegex.ascii_and_accent
+
+    validate_size_of island, max: 24
+    validate_format_of island, with: ValidationRegex.ascii_and_accent
+
+    validate_size_of county, max: 2
+    validate_inclusion_of country, in: Countries.all_alpha_2
+
+    validate_size_of state_provice, max: 30
+    validate_format_of state_provice, with: ValidationRegex.ascii_and_accent
+
+    validate_size_of county, max: 30
+    validate_format_of county, with: ValidationRegex.ascii_and_accent
+
+    validate_size_of municipality, max: 50
+    validate_format_of municipality, with: ValidationRegex.ascii_and_accent
+
+    validate_size_of verbatim_locality, max: 200
+
+    validate_size_of japanese_place_name, max: 14
+
+    validate_size_of japanese_place_name_detail, max: 50
+
+    validate_size_of note, max: 200
+
     Base64Image.save_base64_images(image1, image2, image3, image4, image5)
   end
 end
