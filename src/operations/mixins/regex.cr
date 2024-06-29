1425dbe34b36e4716f0cf4d7e41456d87ffe8da7
fujikawahiroaki <fhir0aki3@gmail.com>
2024-06-29 18:58:12 +0900

    fix: 文字列バリデーション正規表現で0文字が許容されていないバグを修正

--- src/operations/mixins/regex.cr
+++ src/operations/mixins/regex.cr
@@ -1,7 +1,12 @@
 module ValidationRegex
-  @@ascii = /^[!-~ ]+$/
+  @@ascii = /^[!-~ ]*$/
+  @@ascii_and_accent = /^[!-~ À-ÖØ-öø-ÿāīūēōȳĀĪŪĒŌȲ]*$/
 
   def self.ascii
     @@ascii
+  end
+
+  def self.ascii_and_accent
+    @@ascii_and_accent
   end
 end
