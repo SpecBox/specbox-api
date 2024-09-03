514eaef3c6a4b1da5bc62501b14a58f8ae299613
fujikawahiroaki <fhir0aki3@gmail.com>
2024-09-04 05:34:14 +0900

    fix: sentence case, lower caseの正規表現を追加

--- src/operations/mixins/regex.cr
+++ src/operations/mixins/regex.cr
@@ -1,6 +1,8 @@
 module ValidationRegex
   @@ascii = /^[!-~ ]*$/
   @@ascii_and_accent = /^[!-~ À-ÖØ-öø-ÿāīūēōȳĀĪŪĒŌȲ]*$/
+  @@sentence_case_word = /^([A-Z]{1}[a-z]*){0,1}$/
+  @@lower_case_word = /^[a-z-]*$/
 
   def self.ascii
     @@ascii
@@ -8,5 +10,13 @@ module ValidationRegex
 
   def self.ascii_and_accent
     @@ascii_and_accent
+  end
+
+  def self.sentence_case_word
+    @@sentence_case_word
+  end
+
+  def self.lower_case_word
+    @@lower_case_word
   end
 end
