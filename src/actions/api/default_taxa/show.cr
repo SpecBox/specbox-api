8c3d084f0dcf014de2d41631109816e9c120ec3d
fujikawahiroaki <fhir0aki3@gmail.com>
2024-09-04 09:40:52 +0900

    update: DefaultTaxonはあらかじめTaxonとJOINしてからshow #4

--- src/actions/api/default_taxa/show.cr
+++ src/actions/api/default_taxa/show.cr
@@ -1,8 +1,7 @@
 # デフォルト分類情報のID指定取得API
 class Api::DefaultTaxa::Show < ApiAction
   get "/api/default-taxa/:record_id" do
-    record = DefaultTaxonQuery.new.find(record_id)
-    taxon = TaxonQuery.new.find(record.taxon_ptr_id)
-    json TaxonSerializer.nested_key_data(taxon)
+    record = TaxonQuery.new.only_default_taxon.find(record_id)
+    json TaxonSerializer.nested_key_data(record)
   end
 end
