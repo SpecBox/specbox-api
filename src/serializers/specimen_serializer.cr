6e59935a77322e8e2e6ea5fccdba89867a1d044e
fujikawahiroaki <fhir0aki3@gmail.com>
2024-09-08 07:46:24 +0900

    feat: バルクアップデート用標本シリアライザを作成 #2

--- src/serializers/specimen_serializer.cr
+++ src/serializers/specimen_serializer.cr
@@ -1,4 +1,7 @@
-# 採集地点のシリアライザ
+require "./mixins/switch_avram_nothing"
+require "./mixins/simple_date_converter"
+
+# 標本のシリアライザ
 class SpecimenSerializer < BaseSerializer
   def initialize(@specimen : Specimen)
   end
@@ -38,4 +41,79 @@ class SpecimenSerializer < BaseSerializer
       image5:                @specimen.image5,
     }
   end
+end
+
+# JSONから標本へのシリアライズ用クラス
+class SerializedSpecimenJsonForBulkUpdate
+  include JSON::Serializable
+  include JSON::Serializable::Strict
+
+  property ids : Array(UUID)
+  property data : SerializedSpecimenForBulkUpdate
+end
+
+@[JSON::Serializable::Options(emit_nulls: true)]
+class SerializedSpecimenForBulkUpdate
+  include JSON::Serializable
+
+  property custom_taxon_info_id : UUID?
+  property default_taxon_info_id : UUID?
+  property collect_point_info_id : UUID?
+  property collection_settings_info_id : UUID?
+  property tour_id : UUID?
+  property collection_code : Int32?
+  property identified_by : String?
+  @[JSON::Field(converter: SimpleDateConverter.simple_date_converter)]
+  property date_identified : Time?
+  property collecter : String?
+  property year : Int32?
+  property month : Int32?
+  property day : Int32?
+  property sex : String?
+  property preparation_type : String?
+  property disposition : String?
+  property sampling_protocol : String?
+  property sampling_effort : String?
+  property lifestage : String?
+  property establishment_means : String?
+  property rights : String?
+  property allow_kojin_shuzo : Bool?
+  property published_kojin_shuzo : Bool?
+  property note : String?
+  property image1 : String?
+  property image2 : String?
+  property image3 : String?
+  property image4 : String?
+  property image5 : String?
+
+  switch_avram_nothing_for_serialize_class(
+    custom_taxon_info_id,
+    default_taxon_info_id,
+    collect_point_info_id,
+    collection_settings_info_id,
+    tour_id,
+    collection_code,
+    identified_by,
+    date_identified,
+    collecter,
+    year,
+    month,
+    day,
+    sex,
+    preparation_type,
+    disposition,
+    sampling_protocol,
+    sampling_effort,
+    lifestage,
+    establishment_means,
+    rights,
+    allow_kojin_shuzo,
+    published_kojin_shuzo,
+    note,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5
+  )
 end
